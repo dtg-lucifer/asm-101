7e2e42f7430ffb1f31952e26163e6768be76f5f3
devpiush <dev.bosepiush@gmail.com>
2025-07-19 18:13:11 +0530

    feat(code): added code to get user input and then print it

---
+++ 03_user_input/main.asm
@@ -0,0 +1,64 @@
+;---------------------------------------------------------------------------
+; Author  - Piush Bose <dev.bosepiush@gmail.com>
+; Subject - A simple assembly program to take user's name as input
+;           then greet the user with the name
+;---------------------------------------------------------------------------
+
+section                 .data                                 ; setion for storing variables
+text:                   db    "Hey, what is your name?", 10   ; reserve the text to be printed
+text_len:               equ   $-text                          ; get the length
+greetings:              db    "Hello, ",                      ; reserve the greeting text
+greetings_len:          equ   $-greetings                     ; also with the length
+
+
+section                 .bss                                  ; section for uninitialized data
+name:                   resb  16                              ; reserve 16 bytes for the name
+
+
+section                 .text                                 ; section for storing the actual code
+global                  _start                                ; entry point to the label
+
+
+_start:                 call _print_text                      ; calling the print function
+                        call _get_name
+                        call _print_greetings
+                        call _print_name
+
+                        mov       rax, 60                     ; system call for exit
+                        mov       rdi, 0                      ; exit code 0
+                        syscall
+
+
+; --------------------------------------------------------------------------------------------------------
+; Custom sub routines
+; --------------------------------------------------------------------------------------------------------
+_print_text:            mov       rax, 1                      ; function to print hello world
+                        mov       rdi, 1                      ; set the file descriptor to the stdout
+                        mov       rsi, text                   ; address of the string to the output
+                        mov       rdx, text_len               ; number of bytes
+                        syscall                               ; execute the operation
+                        ret                                   ; return to the caller
+
+_get_name:              mov       rax, 0                      ; set it to read mode
+                        mov       rdi, 0                      ; read from the stdin
+                        mov       rsi, name                   ; name buffer to print
+                        mov       rdx, 16                     ; size of the buffer
+                        syscall
+                        mov       rbx, rax                    ; save the number of bytes got from the user
+                        ret
+
+
+_print_greetings:       mov       rax, 1                      ; set it to write
+                        mov       rdi, 1                      ; set it to print to stdout file descriptor
+                        mov       rsi, greetings              ; address of the greetings text
+                        mov       rdx, greetings_len          ; count of the buffer
+                        syscall
+                        ret
+
+
+_print_name:            mov       rax, 1                      ; set it to write
+                        mov       rdi, 1                      ; set it to write to stdout
+                        mov       rsi, name                   ; get the address of the name
+                        mov       rdx, 16                     ; size of the name 
+                        syscall
+                        ret
